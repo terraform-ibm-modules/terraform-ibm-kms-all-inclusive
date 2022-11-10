324ace94fec5127df84ddbc60f115bd1f9ae2e6d
Daniel Butler <dannyeb@gmail.com>
2022-11-10 15:05:43 +0000

    fix: expose endopints

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "key_protect" {
   source            = "git::https://github.com/terraform-ibm-modules/terraform-ibm-key-protect.git?ref=v1.0.0"
   key_protect_name  = var.key_protect_instance_name != null ? var.key_protect_instance_name : "${var.prefix}-kp"
   region            = var.region
+  service_endpoints = var.key_protect_endpoint_type
   resource_group_id = var.resource_group_id
   tags              = var.resource_tags
   metrics_enabled   = var.enable_metrics
@@ -30,10 +31,11 @@ data "ibm_resource_instance" "existing_key_protect" {
 ##############################################################################
 
 module "key_protect_key_rings" {
-  source      = "git::https://github.com/terraform-ibm-modules/terraform-ibm-key-protect-key-ring.git?ref=v1.0.0"
-  for_each    = var.key_map
-  instance_id = local.key_protect_guid
-  key_ring_id = each.key == "default" ? "${var.prefix}-default" : each.key
+  source        = "git::https://github.com/terraform-ibm-modules/terraform-ibm-key-protect-key-ring.git?ref=v1.0.0"
+  for_each      = var.key_map
+  instance_id   = local.key_protect_guid
+  endpoint_type = var.key_ring_endpoint_type
+  key_ring_id   = each.key == "default" ? "${var.prefix}-default" : each.key
 }
 
 data "ibm_kms_key_rings" "existing_key_rings" {
@@ -78,6 +80,7 @@ module "key_protect_keys" {
   source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-key-protect-key.git?ref=v1.0.0"
   # This for_each is needed to assign a name to the maps in the array so they can be referenced/saved in the terraform graph
   for_each                = { for map_name in local.key_ring_key_map : "${map_name.key_ring_name}.${map_name.key_name}" => map_name }
+  endpoint_type           = var.key_endpoint_type
   key_protect_instance_id = local.key_protect_guid
   key_name                = each.value.key_name
   key_protect_key_ring_id = each.value.key_ring_name
@@ -93,6 +96,7 @@ module "existing_key_ring_keys" {
   # This for_each is needed to assign a name to the maps in the array so they can be referenced/saved in the terraform graph
   for_each                = { for map_name in local.existing_key_ring_key_map : "existing-key-ring.${map_name.key_name}" => map_name }
   key_protect_instance_id = local.key_protect_guid
+  endpoint_type           = var.key_endpoint_type
   key_name                = each.value.key_name
   key_protect_key_ring_id = each.value.key_ring_name
   force_delete            = true
--- variables.tf
+++ variables.tf
@@ -35,15 +35,45 @@ variable "create_key_protect_instance" {
   default     = true
 }
 
+variable "key_protect_endpoint_type" {
+  type        = string
+  description = "The type of endpoint to be used, if creating instances for keys and key rings, accepts 'public' or 'private'"
+  default     = "public-and-private"
+  validation {
+    condition     = can(regex("public|private|public-and-private", var.key_protect_endpoint_type))
+    error_message = "The endpoint_type value must be 'public', 'private' or 'public-and-private'."
+  }
+}
+
 variable "use_existing_key_rings" {
   type        = bool
   description = "A flag to control whether the 'existing_key_map' variable is used, defaults to false."
   default     = false
 }
 
+variable "key_ring_endpoint_type" {
+  type        = string
+  description = "The type of endpoint to be used, if creating instances for key rings, accepts 'public' or 'private'"
+  default     = "public"
+  validation {
+    condition     = can(regex("public|private", var.key_ring_endpoint_type))
+    error_message = "The endpoint_type value must be 'public' or 'private'."
+  }
+}
+
 variable "key_map" {
   type        = map(list(string))
   description = "A Map containing the desired Key Ring Names as the keys of the map and a list of desired Key Protect Key names as the values for each Key Ring."
+}
+
+variable "key_endpoint_type" {
+  type        = string
+  description = "The type of endpoint to be used for keys, accepts 'public' or 'private'"
+  default     = "public"
+  validation {
+    condition     = can(regex("public|private", var.key_endpoint_type))
+    error_message = "The endpoint_type value must be 'public' or 'private'."
+  }
 }
 
 variable "existing_key_map" {
