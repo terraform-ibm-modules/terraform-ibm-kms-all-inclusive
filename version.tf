c1bae6a80704e306065c06b00f8ca80cab7f3d71
Conall O'Cofaigh <ocofaigh@ie.ibm.com>
2022-12-22 15:15:17 +0000

    SKIP UPGRADE TEST: due to the fact the example no longer requires the restapi

--- version.tf
+++ version.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
+    # Use "greater than or equal to" range in modules
     ibm = {
       source = "IBM-Cloud/ibm"
-      # Use "greater than or equal to" range in modules
       version = ">= 1.48.0"
     }
-    # ignore linter error, restapi provider needed for key-protect-module/aliases when consuming key-protect-all-inclusive-module
+    # ignore linter error - restapi provider needed for key-protect-module/aliases when consuming key-protect-all-inclusive-module
     # tflint-ignore: terraform_unused_required_providers
     restapi = {
       source  = "Mastercard/restapi"
