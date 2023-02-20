0f46fddb37adc05087435f4cde771ce182abcc65
Matthew Lemmond <Matthew.Lemmond@ibm.com>
2023-02-20 12:31:40 -0500

    feat: remove deprecated RestAPI provider

--- version.tf
+++ version.tf
@@ -9,11 +9,5 @@ terraform {
       source  = "IBM-Cloud/ibm"
       version = ">= 1.49.0"
     }
-    # ignore linter error - include providers required by all child modules
-    # tflint-ignore: terraform_unused_required_providers
-    restapi = {
-      source  = "Mastercard/restapi"
-      version = ">= 1.18.0"
-    }
   }
 }
