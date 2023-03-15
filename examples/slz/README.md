ecfa153dc3a1c9f57a03f944909934f1842ebddb
Matthew Lemmond <Matthew.Lemmond@ibm.com>
2023-03-15 19:25:28 -0400

    chore: add example with SLZ defaults (#245)

---
+++ examples/slz/README.md
@@ -0,0 +1,8 @@
+# Example with SLZ default values
+
+An end-to-end example that uses SLZ's default Key Protect variable values.
+This example will:
+- Create a new resource group (if existing one is not passed in).
+- Create a new Key Protect instance in the region provided.
+- Enable metrics on the Key Protect instance.
+- Create a new key ring and keys on the Key Protect instance.
---
+++ examples/slz/outputs.tf
@@ -0,0 +1,38 @@
+##############################################################################
+# Outputs
+##############################################################################
+
+output "resource_group_name" {
+  description = "Resource group name"
+  value       = module.resource_group.resource_group_name
+}
+
+output "resource_group_id" {
+  description = "Resource group ID"
+  value       = module.resource_group.resource_group_id
+}
+
+output "key_protect_guid" {
+  description = "Key Protect GUID"
+  value       = module.key_protect_all_inclusive.key_protect_guid
+}
+
+output "key_protect_name" {
+  description = "Key Protect Name"
+  value       = module.key_protect_all_inclusive.key_protect_name
+}
+
+output "key_protect_instance_policies" {
+  description = "Instance Polices of the Key Protect instance"
+  value       = module.key_protect_all_inclusive.key_protect_instance_policies
+}
+
+output "key_rings" {
+  description = "IDs of Key Rings created by the module"
+  value       = module.key_protect_all_inclusive.key_rings
+}
+
+output "keys" {
+  description = "Keys created by the module"
+  value       = module.key_protect_all_inclusive.keys
+}
