0306ce8125590d6c4e971bc69e255b47cddafbb9
Matthew Lemmond <Matthew.Lemmond@ibm.com>
2023-05-22 13:39:11 -0400

    refactor: ignore updates to instance policies in upgrade

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -13,12 +13,20 @@ const resourceGroup = "geretain-test-key-protect-all-inclusive"
 const defaultExampleDir = "examples/default"
 const existingResourcesExampleDir = "examples/default"
 
+var ignoreUpdates = []string{
+	//remove after merge
+	"module.key_protect_all_inclusive.module.key_protect[0].ibm_kms_instance_policies.key_protect_instance_policies",
+}
+
 func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
 		Testing:       t,
 		TerraformDir:  dir,
 		Prefix:        prefix,
 		ResourceGroup: resourceGroup,
+		IgnoreUpdates: testhelper.Exemptions{
+			List: ignoreUpdates,
+		},
 	})
 
 	return options
