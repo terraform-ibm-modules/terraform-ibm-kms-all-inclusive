7094d2581419df84e78cffb23c1251099cccacae
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-01-31 10:29:51 +0000

    fix(deps): update terraform github.com/terraform-ibm-modules/terraform-ibm-key-protect to v2.5.1

--- examples/existing-resources/main.tf
+++ examples/existing-resources/main.tf
@@ -13,7 +13,7 @@ module "resource_group" {
 # Create Key Protect instance outside of terraform-ibm-key-protect-all-inclusive module
 ##############################################################################
 module "existing_key_protect" {
-  source            = "git::https://github.com/terraform-ibm-modules/terraform-ibm-key-protect.git?ref=v2.4.1"
+  source            = "git::https://github.com/terraform-ibm-modules/terraform-ibm-key-protect.git?ref=v2.5.1"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   tags              = var.resource_tags
