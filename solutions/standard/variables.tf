5a2e2982b9b8fad95b3b7467609ce6cdb4786275
Aditya-ranjan-16 <aditya16ranjan@gmail.com>
2024-12-04 23:45:47 +0530

    feat:Exposing CBRs in DA

--- solutions/standard/variables.tf
+++ solutions/standard/variables.tf
@@ -115,3 +115,29 @@ variable "keys" {
   description = "A list of key ring objects each containing one or more key objects. [Learn more](https://github.com/terraform-ibm-modules/terraform-ibm-kms-all-inclusive/tree/main/solutions/standard/DA-keys.md)."
   default     = []
 }
+
+##############################################################
+# Context-based restriction (CBR)
+##############################################################
+
+variable "cbr_rules" {
+  type = list(object({
+    description = string
+    account_id  = string
+    rule_contexts = list(object({
+      attributes = optional(list(object({
+        name  = string
+        value = string
+    }))) }))
+    enforcement_mode = string
+    operations = optional(list(object({
+      api_types = list(object({
+        api_type_id = string
+      }))
+    })))
+  }))
+  description = "(Optional, list) List of context-based restrictions rules to create"
+  default     = []
+  # Validation happens in the rule module
+  # NOTE: Context-based restrictions rule applies to Key Protect instances and is not supported for HPCS instances
+}
