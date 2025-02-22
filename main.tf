3e958734b68297e96e231222a2531b67c7427e97
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-02-22 08:14:00 +0000

    fix(deps): update terraform terraform-ibm-modules/kms-key/ibm to v1.4.0 (#621)

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ locals {
 # Create Key Rings and Keys
 module "kms_keys" {
   source                   = "terraform-ibm-modules/kms-key/ibm"
-  version                  = "v1.3.1"
+  version                  = "v1.4.0"
   for_each                 = { for obj in local.key_ring_key_list : "${obj.key_ring_name}.${obj.key_name}" => obj }
   endpoint_type            = var.key_endpoint_type
   kms_instance_id          = local.kms_guid
@@ -143,7 +143,7 @@ moved {
 # Create Keys in existing Key Rings
 module "existing_key_ring_keys" {
   source                   = "terraform-ibm-modules/kms-key/ibm"
-  version                  = "v1.3.1"
+  version                  = "v1.4.0"
   for_each                 = { for obj in local.existing_key_ring_key_list : "existing-key-ring.${obj.key_name}" => obj }
   kms_instance_id          = local.kms_guid
   endpoint_type            = var.key_endpoint_type
