536c5366c01776dd14c1280e62d8f86831501a50
knee-na <130502432+knee-na@users.noreply.github.com>
2025-09-25 08:41:34 -0400

    fix: Update catalog content, description of input and output variables as suggested by Content reviewer (#766)
    * Cloud automation for Key Protect content review
    
    * Update variables.tf
    
    * Update outputs.tf
    
    * minor fixes
    
    * fix pre-commit
    
    * minor fix
    
    ---------
    
    Co-authored-by: Khuzaima Shakeel <56439894+Khuzaima05@users.noreply.github.com>
    Co-authored-by: Khuzaima-Shakeel <Khuzaima.Shakeel@ibm.com>

--- variables.tf
+++ variables.tf
@@ -4,66 +4,66 @@
 
 variable "resource_group_id" {
   type        = string
-  description = "The ID of the Resource Group to provision the Key Protect instance in. Not required if 'create_key_protect_instance' is false."
+  description = "The ID of the resource group where the Key Protect instance is created. Not required if 'create_key_protect_instance' is set to `false`."
   default     = null
 }
 
 variable "region" {
   type        = string
-  description = "The IBM Cloud region where all resources will be provisioned."
+  description = "The IBM Cloud region where all resources are provisioned."
 }
 
 variable "create_key_protect_instance" {
   type        = bool
-  description = "A flag to control whether a Key Protect instance is created, defaults to true."
+  description = "A flag to control whether a Key Protect instance is created. The default is `true`."
   default     = true
 
   validation {
     condition     = !var.create_key_protect_instance || var.resource_group_id != null
-    error_message = "A value must be passed for 'resource_group_id' when 'create_key_protect_instance' is true"
+    error_message = "A value must be passed for 'resource_group_id' when 'create_key_protect_instance' is set to `true`."
   }
 }
 
 variable "key_protect_instance_name" {
   type        = string
-  description = "The name to give the Key Protect instance that will be provisioned by this module. Only used if 'create_key_protect_instance' is true."
+  description = "The name to give the Key Protect instance that that is created by this module. Only used if 'create_key_protect_instance' is set to `true`."
   default     = "key-protect"
 }
 
 variable "key_protect_plan" {
   type        = string
-  description = "Plan for the Key Protect instance. Supported values are 'tiered-pricing' and 'cross-region-resiliency'. Only used if 'create_key_protect_instance' is true."
+  description = "Plan for the Key Protect instance. Supported values are 'tiered-pricing' and 'cross-region-resiliency'. Only used if 'create_key_protect_instance' is set to `true`."
   default     = "tiered-pricing"
   # validation performed in terraform-ibm-key-protect module
 }
 
 variable "rotation_enabled" {
   type        = bool
-  description = "If set to true, Key Protect enables a rotation policy on the Key Protect instance. Only used if 'create_key_protect_instance' is true."
+  description = "If set to `true`, a rotation policy is enabled on the Key Protect instance. Only used if 'create_key_protect_instance' is set to `true`."
   default     = true
 }
 
 variable "rotation_interval_month" {
   type        = number
-  description = "Specifies the key rotation time interval in months. Must be between 1 and 12 inclusive. Only used if 'create_key_protect_instance' is true."
+  description = "Specifies how often keys are rotated in months. Value must be between `1` and `12` inclusive. Only used if 'create_key_protect_instance' is set to `true`."
   default     = 1
 }
 
 variable "dual_auth_delete_enabled" {
   type        = bool
-  description = "If set to true, Key Protect enables a dual authorization policy on the instance. Note: Once the dual authorization policy is set on the instance, it cannot be reverted. An instance with dual authorization policy enabled cannot be destroyed using Terraform. Only used if 'create_key_protect_instance' is true."
+  description = "If set to `true`, a dual authorization policy is enabled on the Key Protect instance. After the dual authorization policy is set on the instance, it cannot be reverted. An instance with dual authorization policy enabled cannot be destroyed by using Terraform. Only used if 'create_key_protect_instance' is set to `true`."
   default     = false
 }
 
 variable "enable_metrics" {
   type        = bool
-  description = "Set to true to enable metrics on the Key Protect instance. Only used if 'create_key_protect_instance' is true. In order to view metrics, you will need a Monitoring (Sysdig) instance that is located in the same region as the Key Protect instance. Once you provision the Monitoring instance, you will need to enable platform metrics."
+  description = "Set to `true` to enable metrics on the Key Protect instance. Only used if 'create_key_protect_instance' is set to `true`. In order to view metrics, you need an IBM Cloud Monitoring (Sysdig) instance that is located in the same region as the Key Protect instance. After you provision a Monitoring instance, enable platform metrics to monitor your Key Protect instance."
   default     = true
 }
 
 variable "key_create_import_access_enabled" {
   type        = bool
-  description = "If set to true, Key Protect enables a key create import access policy on the instance. Only used if 'create_key_protect_instance' is true."
+  description = "If set to `true`, a key create and import access policy is enabled on the instance of Key Protect. Only used if 'create_key_protect_instance' is set to `true`."
   default     = true
 }
 
@@ -75,33 +75,33 @@ variable "key_create_import_access_settings" {
     import_standard_key = optional(bool, true)
     enforce_token       = optional(bool, false)
   })
-  description = "Key create import access policy settings to configure if 'enable_key_create_import_access_policy' is true. Only used if 'create_key_protect_instance' is true. For more info see https://cloud.ibm.com/docs/key-protect?topic=key-protect-manage-keyCreateImportAccess"
+  description = "Key create and import access policy settings to configure if 'enable_key_create_import_access_policy' is set to `true`. Only used if 'create_key_protect_instance' is set to `true`. For more info see https://cloud.ibm.com/docs/key-protect?topic=key-protect-manage-keyCreateImportAccess"
   default     = {}
 }
 
 variable "key_protect_allowed_network" {
   type        = string
-  description = "The type of the allowed network to be set for the Key Protect instance. Possible values are 'private-only', or 'public-and-private'. Only used if 'create_key_protect_instance' is true."
+  description = "Allowed network types for the Key Protect instance. Possible values are 'private-only', or 'public-and-private'. Only used if 'create_key_protect_instance' is set to `true`."
   default     = "public-and-private"
   validation {
     condition     = can(regex("private-only|public-and-private", var.key_protect_allowed_network))
-    error_message = "The key_protect_allowed_network value must be 'private-only' or 'public-and-private'."
+    error_message = "The `key_protect_allowed_network` value must be 'private-only' or 'public-and-private'."
   }
 }
 
 variable "existing_kms_instance_crn" {
   type        = string
-  description = "The CRN of an existing Key Protect or Hyper Protect Crypto Services instance. Required if 'create_key_protect_instance' is false."
+  description = "The CRN of an existing Key Protect or Hyper Protect Crypto Services instance. Required if 'create_key_protect_instance' is set to `false`."
   default     = null
 
   validation {
     condition     = !(var.create_key_protect_instance && var.existing_kms_instance_crn != null)
-    error_message = "'create_key_protect_instance' cannot be true when passing a value for 'existing_kms_instance_crn'"
+    error_message = "'create_key_protect_instance' cannot be set to `true` when passing a value for 'existing_kms_instance_crn'."
   }
 
   validation {
     condition     = var.create_key_protect_instance || var.existing_kms_instance_crn != null
-    error_message = "A value must be provided for 'existing_kms_instance_crn' when 'create_key_protect_instance' is false"
+    error_message = "A value must be provided for 'existing_kms_instance_crn' when 'create_key_protect_instance' is set to `false`."
   }
 }
 
@@ -125,40 +125,40 @@ variable "keys" {
       })), [])
     }))
   }))
-  description = "A list of objects which contain the key ring name, a flag indicating if this key ring already exists, and a flag to enable force deletion of the key ring. In addition, this object contains a list of keys with all of the information on the keys to be created in that key ring."
+  description = "A list of objects that contain the key ring name, a flag indicating if this key ring already exists, and a flag to enable force deletion of the key ring. This object also contains a list of keys to be created in that key ring."
   sensitive   = true
   default     = []
 }
 
 variable "key_ring_endpoint_type" {
   type        = string
-  description = "The type of endpoint to be used for creating key rings. Accepts 'public' or 'private'"
+  description = "The type of endpoint to be used for creating key rings. Possible values are 'public' or 'private'."
   default     = "public"
   validation {
     condition     = can(regex("^(public|private)$", var.key_ring_endpoint_type))
-    error_message = "The endpoint_type value must be 'public' or 'private'."
+    error_message = "The value isn't valid. Possible values are 'public' or 'private'."
   }
 }
 
 variable "key_endpoint_type" {
   type        = string
-  description = "The type of endpoint to be used for creating keys. Accepts 'public' or 'private'"
+  description = "The type of endpoint to be used for creating keys. Possible values are 'public' or 'private'"
   default     = "public"
   validation {
     condition     = can(regex("^(public|private)$", var.key_endpoint_type))
-    error_message = "The endpoint_type value must be 'public' or 'private'."
+    error_message = "The value isn't valid. Possible values are 'public' or 'private'."
   }
 }
 
 variable "resource_tags" {
   type        = list(string)
-  description = "Optional list of tags to be added to the Key Protect instance. Only used if 'create_key_protect_instance' is true."
+  description = "Optional list of tags to add to the Key Protect instance. Only used if 'create_key_protect_instance' is set to `true`."
   default     = []
 }
 
 variable "access_tags" {
   type        = list(string)
-  description = "A list of access tags to apply to the Key Protect instance created by the module. Only used if 'create_key_protect_instance' is true."
+  description = "A list of access tags to apply to the Key Protect instance. Only used if 'create_key_protect_instance' is set to `true`."
   default     = []
 }
 
@@ -182,14 +182,14 @@ variable "cbr_rules" {
       }))
     })))
   }))
-  description = "(Optional, list) List of context-based restrictions rules to create"
+  description = "(Optional, list) List of context-based restriction rules to create"
   default     = []
   # Validation happens in the rule module
-  # NOTE: Context-based restrictions rule applies to Key Protect instances and is not supported for HPCS instances
+  # NOTE: Context-based restriction rules applies to Key Protect instances only and is not supported by Hyper Protect Crypto Services (HPCS) instances
 
   validation {
     # condition     = (length(regexall(".*hs-crypto.*", var.existing_kms_instance_crn)) > 0 && length(var.cbr_rules) == 0) || (length(regexall(".*kms.*", var.existing_kms_instance_crn)) > 0) || (var.existing_kms_instance_crn == null)
     condition     = var.existing_kms_instance_crn == null ? true : length(regexall(".*hscrypto.*", var.existing_kms_instance_crn)) > 0 ? length(var.cbr_rules) == 0 : true
-    error_message = "When passing an HPCS instance as value for `existing_kms_instance_crn` you cannot provide `cbr_rules`. Context-based restrictions is not supported for HPCS instances. For more information, see https://cloud.ibm.com/docs/account?topic=account-context-restrictions-whatis#cbr-adopters for the services that supports CBR."
+    error_message = "When passing a Hyper Protect Crypto Services (HPCS) instance as a value for `existing_kms_instance_crn` you cannot provide `cbr_rules`. Context-based restrictions are not supported by HPCS instances. For more information, go to [services that integrate with context-based restrictions](https://cloud.ibm.com/docs/account?topic=account-context-restrictions-whatis#cbr-adopters)."
   }
 }
