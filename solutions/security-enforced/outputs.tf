2db3af576a0258408c36c5ca463be5a8dd46834b
raagamounica <mounica.tellabati@ibm.com>
2025-11-04 12:49:41 +0530

    feat: Added next step url for key protect (fc+se)

--- solutions/security-enforced/outputs.tf
+++ solutions/security-enforced/outputs.tf
@@ -67,27 +67,27 @@ output "kms_public_endpoint" {
 ##############################################################################
 
 output "next_steps_text" {
-  value       = "module.security_enforced.next_steps_text"
+  value       = module.security_enforced.next_steps_text
   description = "Next steps text"
 }
 
 output "next_step_primary_label" {
-  value       = "module.security_enforced.next_step_primary_label"
+  value       = module.security_enforced.next_step_primary_label
   description = "Primary label"
 }
 
 output "next_step_primary_url" {
-  value       = "module.security_enforced.next_step_primary_url" 
+  value       = module.security_enforced.next_step_primary_url 
   description = "Primary URL"
 }
 
 output "next_step_secondary_label" {
-  value       = "module.security_enforced.next_step_secondary_label"
+  value       = module.security_enforced.next_step_secondary_label
   description = "Secondary label"
 }
 
 output "next_step_secondary_url" {
-  value       = " module.security_enforced.next_step_secondary_url"
+  value       =  module.security_enforced.next_step_secondary_url
   description = "Secondary URL"
 }
 
