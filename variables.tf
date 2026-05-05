17b3532a1344e28e41e45a5c5ce3a7f49d25297c
iamar7 <iamar7@users.noreply.github.com>
2026-05-05 11:30:13 +0530

    fix: standardize resource_tags variable, update descriptions and add access tag validation

--- variables.tf
+++ variables.tf
@@ -152,13 +152,17 @@ variable "key_endpoint_type" {
 
 variable "resource_tags" {
   type        = list(string)
-  description = "Optional list of tags to add to the Key Protect instance. Only used if 'create_key_protect_instance' is set to `true`."
+  description = "Add user resource tags to the Key Protect instance instance to organize, track, and manage costs. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#tag-types)."
   default     = []
+  validation {
+    condition     = alltrue([for tag in var.resource_tags : can(regex("^[A-Za-z0-9 _\\-.:](1, 128)$", tag))])
+    error_message = "Each resource tag must be 128 characters or less and may contain only A-Z, a-z, 0-9, spaces, underscore (_), hyphen (-), period (.), and colon (:)."
+  }
 }
 
 variable "access_tags" {
   type        = list(string)
-  description = "A list of access tags to apply to the Key Protect instance. Only used if 'create_key_protect_instance' is set to `true`."
+  description = "Add access management tags to the Key Protect instance instance to control access. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#create-access-console)."
   default     = []
 }
 
